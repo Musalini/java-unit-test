7aa07e96d0d0bc70c7b6fe7796ba0d7f6f6dd78c
Musa Muradzade <muradzade.musa@gmail.com>
2024-11-04 22:05:18 +0300

    changed jdk version in pom.xml to 17

---
+++ src/main/java/Program.java
@@ -0,0 +1,11 @@
+package src.main.java;
+
+public class Program {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+    public boolean checkIsAdult(int i) {
+        return i >= 18;
+    }
+}
